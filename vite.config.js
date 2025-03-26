27c5d54aac9bc59e225f46baeca55624f738584f
ConeAnt <antdyboll@gmail.com>
2025-03-26 12:25:17 +0000

    fixed pacjkages homepage tag

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',  // You can specify the output directory here if you want 'build' instead of 'dist'
+  }
 })
